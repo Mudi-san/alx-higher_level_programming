d6834d881ab48d05558794de2596789d061997fc
Mudi-san <mudijewilly@gmail.com>
2023-09-07 07:11:20 -0700

    pyimport mudi

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
 argc = len(sys.argv) - 1
 if (argc == 0):
     print(argc, "arguments.")
+elif (argc == 1):
+    print(argc, "argument:".format(sys.argv[0]))
+    print("1:", sys.argv[1])
 else:
     print(argc, "arguments:")
     for i in range(1, argc + 1):
