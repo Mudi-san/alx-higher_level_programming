27189f478671f4437558b9e1bb4e12d1d5539093
Mudi-san <mudijewilly@gmail.com>
2023-09-07 06:00:31 -0700

    pyimport mudi

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    a = 1
-    b = 2
-    c = add(a, b)
-    print(F"{a} + {b} = {c}")
+
+a = 1
+b = 2
+c = add(a, b)
+print(F"{a} + {b} = {c}")
