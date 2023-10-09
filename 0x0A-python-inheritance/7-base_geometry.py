c29587df74a2174fa405207e2bb5ca0010890755
Mudi-san <mudijewilly@gmail.com>
2023-10-09 10:52:04 -0700

    Mudi inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-""" creating a class """
+"""
+Module 7-base_geometry
+Contains BaseGeometry
+with public instance method area and integer_validation
+"""
 
 
 class BaseGeometry:
     """
-    BaseGeometry class
+    Methods:
+        area(self)
+        integer_validator(self, name, value)
     """
     def area(self):
-        """
-        area method
-        """
+        """not implemented"""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates input
+        Args:
+            name (str): assumed always a string
+            value (int): greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
