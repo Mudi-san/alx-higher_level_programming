4bbc2dda1621fb4fb10ce69143ad5d71ae42211a
Mudi-san <mudijewilly@gmail.com>
2023-10-10 07:57:34 -0700

    Mudi done File IO

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """writes an Object to a text file, using a JSON representation"""
-import json
-
 
 def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation"""
-    with open(filename, 'w') as f:
-        return f.write(json.dumps(my_obj))
+    """Writes Python obj to file using JSON represenation
+    Args:
+        my_obj: python object
+        filename: file
+    """
+    import json
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
