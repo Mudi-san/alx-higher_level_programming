7fe5f3e4e79f15eecc778d01f56c938ceecad06b
Mudi-san <mudijewilly@gmail.com>
2023-10-10 08:13:56 -0700

    Mudi done File IO

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,12 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
+arglist = list(sys.argv[1:])
 
 try:
-    json_list = load_from_json_file(filename)
-except:
+    json_list = load_from_json_file('add_item.json')
+except Exception:
     json_list = []
 
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+json_list.extend(arglist)
+save_to_json_file(json_list, 'add_item.json')
