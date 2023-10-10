83d2310ecb99e7ba1965d20ba6579b94ac8a3121
Mudi-san <mudijewilly@gmail.com>
2023-10-10 08:00:52 -0700

    Mudi done File IO

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""writes an Object to a text file, using a JSON representation"""
+"""
+Module 7-save_to_json_file
+Contains function that writes Python obj to file using JSON represenation
+"""
+
 
 def save_to_json_file(my_obj, filename):
     """Writes Python obj to file using JSON represenation
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""function that creates an Object from a “JSON file”"""
+"""
+Module 8-load_from_json_file
+Contains function that creates a Python obj from JSON file
+"""
 
 
 def load_from_json_file(filename):
-    """function that creates an Object from a “JSON file”"""
-import json
-    with open(filename, 'r') as f:
+    """Creates a Python obj from JSON file
+    Args:
+        filename: file
+    """
+    import json
+
+    with open(filename, mode="r", encoding="utf-8") as f:
         return json.load(f)
