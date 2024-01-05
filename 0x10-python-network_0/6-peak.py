690dc3fcfd941dd10d8c9cd87011a7164b86a451
Mudi-san <mudijewilly@gmail.com>
2024-01-05 23:37:53 +0200

    Mudi is Done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 """ This module returns the peak of the list
 """
-
-
 def find_peak(list_of_integers):
-    """ This function returns the peak of the list
-    """
-    if (len(list_of_integers) == 0):
-        return None
-
-    else:
-        peak = list_of_integers[0]
-        for i in range(len(list_of_integers)):
-            if list_of_integers[i] > peak:
+    if list_of_integers:
+        for i in range(len(list_of_integers) - 1):
+            if list_of_integers[i] >= list_of_integers[i + 1] and list_of_integers[i] >= list_of_integers[i - 1]:
                 peak = list_of_integers[i]
-        return peak
+                return peak
+    else:
+        return None
